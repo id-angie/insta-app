e3480ed1a810b19bc4f0fe1e96c0dc9db3c2ab11
Angie <id_angie@bk.ru>
2018-09-10 02:01:15 +0300

    Configure moment.js locales

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+import moment from 'moment';
+import 'moment/locale/ru';
 import ProfilePage from './ProfilePage.js';
+moment.locale('ru');
 
 const App = () => (
   <div className="App">
