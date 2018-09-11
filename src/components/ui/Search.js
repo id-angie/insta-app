3686b5fdd0e6ade76311f0ee2d055382886b5c97
zamarawka <somebody.told@mail.ru>
2018-09-12 02:06:04 +0300

    Add react-select example

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -4,6 +4,32 @@ import Select from 'react-select';
 import users from '../../users.json';
 import './Search.css';
 
+// const ClearIndicator  = (props) => {
+//   const {
+//     innerProps: { ref, ...restInnerProps },
+//     children,
+//     getStyles
+//   } = props;
+
+//   return (
+//     <div
+//       {...restInnerProps}
+//       ref={ref}
+//       style={getStyles('clearIndicator', props)}
+//       className="search__close"
+//     >
+//       {children}
+//     </div>
+//   );
+// }
+
+const ClearIndicator  = ({ innerProps: { ref, ...restInnerProps } }) => (
+  <div
+    {...restInnerProps}
+    className="search__close"
+  />
+);
+
 class Search extends Component {
   state = {
     isFocused: false,
@@ -28,11 +54,13 @@ class Search extends Component {
 
     const { selectedOption } = this.state;
 
+
     return (
       <div className={cn("search", {
             "search_focused": this.state.isFocused
           })}>
         <Select
+          isClearable
           value={selectedOption}
           options={options}
           classNamePrefix="search__select"
@@ -41,11 +69,9 @@ class Search extends Component {
           onFocus={() => this.setState({ isFocused: true })}
           onBlur={this.resetSearch}
           placeholder="Поиск"
+          components={{ ClearIndicator }}
         />
-        <div
-          className="search__close"
-          onClick={this.resetSearch}
-        />
+
       </div>
     );
   }
