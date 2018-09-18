1865eb7e5daf35e75b71569c4898bca886286c52
Angie <id_angie@bk.ru>
2018-09-18 19:55:29 +0300

    Footer list fixes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,19 +4,19 @@ import './index.css';
 const Footer = () => (
 <div className="footer">
   <div className="container">
-    <div className="footer__list">
-      <div>
+    <div className="footer__block">
+      <div className="footer__list">
         <a href="./" className="footer__link">О нас</a>
-        <a href="./" className="footer__link ml20">Поддержка</a>
-        <a href="./" className="footer__link ml20">Пресса</a>
-        <a href="./" className="footer__link ml20">Api</a>
-        <a href="./" className="footer__link ml20">Вакансии</a>
-        <a href="./" className="footer__link ml20">Конфеденциальность</a>
-        <a href="./" className="footer__link ml20">Условия</a>
-        <a href="./" className="footer__link ml20">Директория</a>
-        <a href="./" className="footer__link ml20">Профили</a>
-        <a href="./" className="footer__link ml20">Хэштеги</a>
-        <a href="./" className="footer__link ml20">Язык</a>
+        <a href="./" className="footer__link">Поддержка</a>
+        <a href="./" className="footer__link">Пресса</a>
+        <a href="./" className="footer__link">Api</a>
+        <a href="./" className="footer__link">Вакансии</a>
+        <a href="./" className="footer__link">Конфеденциальность</a>
+        <a href="./" className="footer__link">Условия</a>
+        <a href="./" className="footer__link">Директория</a>
+        <a href="./" className="footer__link">Профили</a>
+        <a href="./" className="footer__link">Хэштеги</a>
+        <a href="./" className="footer__link">Язык</a>
       </div>
       <div className="footer__copyright">
         <span>© 2018 Angiegram</span>
