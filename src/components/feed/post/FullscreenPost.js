3cb9bf67981edac50700760a10338dbb32750304
Angie <id_angie@bk.ru>
2018-10-03 19:18:56 +0300

    Modal fixes in fullscreen posts

--- src/components/feed/post/FullscreenPost.js
+++ src/components/feed/post/FullscreenPost.js
@@ -1,37 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './FullscreenPost.css';
 import PostInfo from './PostInfo.js';
 
-const FullscreenPost = (props) => (
-  <div>
-    <div className="modal">
-    </div>
-    <div className="fullscreen-post-container" onClick={props.closeFullscreen}>
-      <div
-        className="fullscreen-post__navigation fullscreen-post__navigation_previous"
-        onClick={() => alert('prev')}
-      />
-      <div
-        className="fullscreen-post"
-        onClick={ (e) => {e.stopPropagation()} }
-      >
-        <div className="fullscreen-post__img-container fullscreen-post__img-container_fullscreen">
+class FullscreenPost extends Component {
+
+  componentDidMount() {
+    document.body.style.overflow = 'hidden';
+  }
+
+  componentWillUnmount() {
+    document.body.style.overflow = 'auto';
+  }
+
+  render() {
+    return (
+      <div className="modal">
+        <div className="fullscreen-post-container" onClick={this.props.closeFullscreen}>
           <div
-            className="fullscreen-post__img fullscreen-post__img_fullscreen"
-            style={{ backgroundImage: `url(${props.img})` }}
+            className="fullscreen-post__navigation fullscreen-post__navigation_previous"
+            onClick={(e) => this.props.showPrevPost(e, this.props.post.id)}
           />
+          <div
+            className="fullscreen-post"
+            onClick={ (e) => {e.stopPropagation()} }
+          >
+            <div className="fullscreen-post__img-container fullscreen-post__img-container_fullscreen">
+              <div
+                className="fullscreen-post__img fullscreen-post__img_fullscreen"
+                style={{ backgroundImage: `url(${this.props.img})` }}
+              />
+            </div>
+            <PostInfo user={this.props.user} post={this.props.post} handleClick={this.props.handleClick} img={this.props.img} />
+          </div>
+          <div
+            className="fullscreen-post__navigation fullscreen-post__navigation_next"
+            onClick={(e) => this.props.showNextPost(e, this.props.post.id)}
+          />
+          <button className="fullscreen-post__navigation fullscreen-post__navigation_close">
+            <span>×</span>
+          </button>
         </div>
-        <PostInfo user={props.user} post={props.post} handleClick={props.handleClick} img={props.img} />
       </div>
-      <div
-        className="fullscreen-post__navigation fullscreen-post__navigation_next"
-        onClick={() => alert('next')}
-      />
-      <button className="fullscreen-post__navigation fullscreen-post__navigation_close">
-        <span>×</span>
-      </button>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default FullscreenPost;
