1a4c50431b2aa0136c7a5cc2f1f5da5058b5ef4d
Angie <id_angie@bk.ru>
2018-11-13 00:31:16 +0300

    Edit links in Footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,17 +7,17 @@ const Footer = () => (
   <div className="container">
     <div className="footer__block">
       <div className="footer__list">
-        <a href="./" className="footer__link">О нас</a>
-        <a href="./" className="footer__link">Поддержка</a>
-        <a href="./" className="footer__link">Пресса</a>
-        <a href="./" className="footer__link">Api</a>
-        <a href="./" className="footer__link">Вакансии</a>
-        <a href="./" className="footer__link">Конфеденциальность</a>
-        <a href="./" className="footer__link">Условия</a>
-        <a href="./" className="footer__link">Директория</a>
-        <a href="./" className="footer__link">Профили</a>
-        <a href="./" className="footer__link">Хэштеги</a>
-        <a href="./" className="footer__link">Язык</a>
+        <a href="/login" className="footer__link">О нас</a>
+        <a href="/login" className="footer__link">Поддержка</a>
+        <a href="/login" className="footer__link">Пресса</a>
+        <a href="/login" className="footer__link">Api</a>
+        <a href="/login" className="footer__link">Вакансии</a>
+        <a href="/login" className="footer__link">Конфеденциальность</a>
+        <a href="/login" className="footer__link">Условия</a>
+        <a href="/login" className="footer__link">Директория</a>
+        <a href="/login" className="footer__link">Профили</a>
+        <a href="/login" className="footer__link">Хэштеги</a>
+        <a href="/login" className="footer__link">Язык</a>
       </div>
       <div className="footer__copyright">
         <span>© 2018 Angiegram</span>
