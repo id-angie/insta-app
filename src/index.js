7cd61ac69d9e5592af68f5f5290aeb8091b39238
Angie <id_angie@bk.ru>
2018-11-18 03:25:42 +0300

    Use redux-persist package

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.scss';
 import '../node_modules/normalize.css/normalize.css';
 
 import App from './components/App';
-import store from './store';
+import store, { persistor } from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['currentUser']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   applyMiddleware(thunk)
 );
+
+export const persistor = persistStore(store);
 
 export default store;
